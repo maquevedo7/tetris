7edfdee68edb6f3cf18750f7139879ab45798adb
nimorih <mafequevedo1@gmail.com>
2021-08-19 22:51:49 -0500

    avances Zayda

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var context = canvas.getContext("2d") //context para guardar el contexto de gráficos 2D, que es la herramienta  que realmente utilizaremos para dibujar.
